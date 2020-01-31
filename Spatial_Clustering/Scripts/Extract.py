f53256dd5a985b0e3f7d547f0b4f19d11cd94799
Jason Bian <jasonzb@umich.edu>
2020-01-30 19:36:09 -0500

    Finished bluedot for all 4 directions

---
+++ Spatial_Clustering/Scripts/Extract.py
@@ -0,0 +1,44 @@
+import os, sys, time
+
+sys.path.append(os.path.join("usr/local/lib/python2.7/dist-packages (1.3.2)"))
+
+from bluedot import*
+from signal import pause
+from Capture import*
+
+def start():
+    print("Running Capture")
+    x = Sensor()
+    x.erase()
+    x.run()
+    
+def stop():
+    print("Extracting Data")
+    x = Sensor()
+    x.stop()
+    x.extract()
+    print("Extraction Complete")
+
+def calibrate():
+    print("Initial Calibration")
+    x = Sensor()
+    x.calibrate()
+    #light_signal("off")
+
+def senddata():
+    print("Sending ")
+    #light_signal("on")
+          
+def swiped(swipe):
+    if swipe.up:
+        start()
+    elif swipe.down:
+        stop()
+    elif swipe.left:
+        calibrate()
+    elif swipe.right:
+        senddata()
+
+print("Running Extract")
+stop()
+
